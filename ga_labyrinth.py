7b1986e3b572adc49677dbb499a6fdd1a893364c
Mauren <stormqueen1990@gmail.com>
2012-08-20 21:32:01 -0300

    Path class implementation. Path value implementation.

--- ga_labyrinth.py
+++ ga_labyrinth.py
@@ -1,36 +1,97 @@
 #!/usr/bin/env python
-#L N O S
-#0 0 0 0 "0"
-#0 0 0 1 "1"
-#0 0 1 0 "2"
-#0 0 1 1 "3"
-#0 1 0 0 "4"
-#0 1 0 1 "5"
-#0 1 1 0 "6"
-#0 1 1 1 "7"
-#1 0 0 0 "8"
-#1 0 0 1 "9"
-#1 0 1 0 "10"
-#1 0 1 1 "11"
-#1 1 0 0 "12"
-#1 1 0 1 "13"
-#1 1 1 0 "14"
-#1 1 1 1 "15"
+import random
+
 labirinto = [
-				[],
-				[],
-				[],
-				[],
-				[],
-				[],
-				[],
-				[],
-				[],
-				[],
+				[ 6,  5,  5,  5, 12,  6,  5,  5, 12,  6],
+				[10,  7,  5,  5,  9, 10,  6, 12,  3,  9],
+				[ 2,  5,  5,  5,  5,  8, 11,  2,  5, 12],
+				[ 3,  5,  5, 12,  7,  1,  5,  9, 14, 10],
+				[ 6, 13,  6,  8,  6,  5,  5,  5,  9,  3],
+				[ 3,  5,  8,  2,  1,  5,  5,  5,  5, 12],
+				[ 6,  5,  9, 10,  7,  4, 12,  6,  5,  9],
+				[ 2,  4,  5,  1,  4,  8,  2,  3,  5, 12],
+				[10, 10,  7, 13, 10, 10,  3,  5,  5,  8],
+				[ 9,  3,  5,  5,  9,  3,  5,  5,  5,  9],
 			]
 
+DIR_LESTE_STR = "00"
+DIR_NORTE_STR = "01"
+DIR_OESTE_STR = "10"
+DIR_SUL_STR   = "11"
+
+DIRS = [DIR_LESTE_STR
+       ,DIR_NORTE_STR
+	   ,DIR_OESTE_STR
+	   ,DIR_SUL_STR]
+
+DIR_LESTE = 8
+DIR_NORTE = 4
+DIR_OESTE = 2
+DIR_SUL   = 1
+
+MIN_TAM_CAMINHO = 27
+MAX_TAM_CAMINHO = 100
+
+class Caminho:
+	def __init__(self):
+		random.seed()
+		self.tamanho = random.randint(MIN_TAM_CAMINHO, MAX_TAM_CAMINHO)
+		self.custo = 0
+		self.caminho = self.__geraCaminho(self.tamanho)
+
+		print(self.caminho)
+
+	def __geraCaminho(self, tamanho):
+		caminho = []
+		x = 0
+
+		while x < tamanho:
+			item = random.randint(0,3)
+			caminho.append(DIRS[item])
+			x = x + 1
+
+		return caminho
+
+	def __unicode__(self):
+		return u"Caminho[" + `self.tamanho` + ", " + `self.custo` + ", " + `self.caminho` + "]" 
+		
+# Pontua o caminho recebido de acordo com o mapa
 def avaliaCaminho(individuo):
-	
+	valCaminho = 0
+	posAtual = [9,0]
+
+	for str in individuo:
+		if str == DIR_LESTE_STR:
+			if labirinto[posAtual[0]][posAtual[1]] & DIR_LESTE == 0:
+				valCaminho = valCaminho + 10
+			else:
+				valCaminho = valCaminho + 30
+
+			posAtual[1] = posAtual[1] + 1
+		elif str == DIR_NORTE_STR:
+			if labirinto[posAtual[0]][posAtual[1]] & DIR_NORTE == 0:
+				valCaminho = valCaminho + 10
+			else:
+				valCaminho = valCaminho + 30
+
+			posAtual[0] = posAtual[0] - 1
+		elif str == DIR_OESTE_STR:
+			if labirinto[posAtual[0]][posAtual[1]] & DIR_OESTE == 0:
+				valCaminho = valCaminho + 10
+			else:
+				valCaminho = valCaminho + 30
+
+			posAtual[1] = posAtual[1] - 1
+		elif str == DIR_SUL_STR:
+			if labirinto[posAtual[0]][posAtual[1]] & DIR_SUL == 0:
+				valCaminho = valCaminho + 10
+			else:
+				valCaminho = valCaminho + 30
+
+			posAtual[0] = posAtual[0] + 1
+
+	return valCaminho
 
 if __name__ == "__main__":
-	print("maiiiiiiiiiiiiin")
+	caminho = Caminho()
+	print(caminho)
